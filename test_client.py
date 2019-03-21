4b942199f6ce5543109e6d208d847fa58cfe66eb
unknown <Asixsend>
2019-03-21 16:55:52 +0300

    Комментарии к многопоточности

--- test_client.py
+++ test_client.py
@@ -3,7 +3,7 @@ import socket
 
 sock_1 = socket.socket() # создаем сокет
 sock_1.connect( ('localhost', 9090) ) # подключаемся к серверу, где 'localhost ИЛИ IP СЕРВЕРА' а после номер порта
-sock_1.send(b'hello from Vova') # отправка сообщения с переводом в байты
+sock_1.send(b'hello from Vladimir') # отправка сообщения с переводом в байты
 
 data = sock_1.recv(1024) # устанавливаем размер сообщения в 1024 байта
 sock_1.close() # закрытие соединения
